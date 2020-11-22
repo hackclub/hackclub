63a34696bd4445097109c1b82fadbed7d4f9b279
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-11-22 15:22:01 +0800

    Revert "Add images + sample code"
    This reverts commit b27fcde81954793baae9711bca5e413971b474ac.

--- workshops/cpx_reaction_time/src/main.py
+++ workshops/cpx_reaction_time/src/main.py
@@ -14,8 +14,11 @@ while True:
         cp.pixels[4] = colour
         timePassed = 0
         while cp.button_b == False:
+
             sleep(0.001)
+
             timePassed+=0.001
+
         print("Your reaction speed was " + str(timePassed) + " seconds.")
     else:
         cp.pixels[0] = choice(colours)
@@ -23,4 +26,6 @@ while True:
         cp.pixels[2] = choice(colours)
         cp.pixels[3] = choice(colours)
         cp.pixels[4] = choice(colours)
+    
     sleep(2)
+    
